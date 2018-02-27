256c4c575ed0b8fcc1607192654466e1945219c2
Roman Stranghöner <roman.stranghoener@innoq.com>
2018-02-27 14:50:58 +0100

    remove js task and change dependencies

--- faucet.config.js
+++ faucet.config.js
@@ -3,10 +3,6 @@ const path = require('path')
 const config = {
   static: [
     {
-      source: './node_modules/bootstrap/dist/js',
-      target: './dist/js'
-    },
-    {
       source: './dist/css/innoq-bootstrap-theme.css',
       target: './documentation/dist/css/bootstrap.css'
     }
