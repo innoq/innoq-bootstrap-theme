cdc923a9841720f7d49969a0500fcd9fa9beca05
Roman Stranghöner <roman.stranghoener@innoq.com>
2018-02-28 12:20:48 +0100

    use different faucet configs for dev and prod

--- faucet.config.dev.js
+++ faucet.config.dev.js
@@ -1,6 +1,12 @@
 const path = require('path')
 
 const config = {
+  static: [
+    {
+      source: './dist/css/innoq-bootstrap-theme.css',
+      target: './documentation/dist/css/bootstrap.css'
+    }
+  ],
   sass: [
     {
       source: './src/styles/index.scss',
