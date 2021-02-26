366a0c09c3f91da4edec3619a44bac1861ad2503
Roman Stranghöner <roman.stranghoener@innoq.com>
2021-02-26 17:19:41 +0100

    fix faucet config for local development

--- faucet.config.dev.js
+++ faucet.config.dev.js
@@ -1,18 +1,12 @@
 const path = require('path')
 
 const config = {
-  static: [
-    {
-      source: "./dist/css/innoq-bootstrap-theme.css",
-      target: "./documentation/docs/4.6/dist/css",
-    },
-  ],
   sass: [
     {
       source: "./src/styles/index.scss",
-      target: "./dist/css/innoq-bootstrap-theme.css",
+      target: "./documentation/docs/4.6/dist/css/bootstrap.css",
     },
-  ]
+  ],
 };
 
 module.exports = config
