8e343a58fbd3935a11cab15ef20bb56c6968a251
Christoph Wiemers <christoph.wiemers@innoq.com>
2022-02-17 10:08:39 +0100

    bootstrap 5.1 progress

--- faucet.config.dev.js
+++ faucet.config.dev.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require("path");
 
 const config = {
   sass: [
     {
       source: "./src/styles/index.scss",
-      target: "./documentation/docs/4.6/dist/css/bootstrap.css",
+      target: "./documentation/docs/5.1/dist/css/bootstrap.css",
     },
   ],
 };
 
-module.exports = config
+module.exports = config;
